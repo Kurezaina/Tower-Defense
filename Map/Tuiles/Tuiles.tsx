5ab4631ea5c9278c158e40e4da68ebe444359873
Dehousse Robin <robindehousse1@gmail.com>
2024-05-07 21:41:23 +0200

    emplacement

--- Map/Tuiles/Tuiles.tsx
+++ Map/Tuiles/Tuiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tuiles" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Tuiles" tilewidth="32" tileheight="32" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="chemin.png"/>
@@ -12,5 +12,8 @@
  </tile>
  <tile id="3">
   <image width="32" height="32" source="noir.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="emplacemement_faux.png"/>
  </tile>
 </tileset>
