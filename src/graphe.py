a785638189a03aefee17698c089629d114c6afae
Solirs <zcauchemar@gmail.com>
2024-05-28 21:34:54 +0200

    commentaires

--- src/graphe.py
+++ src/graphe.py
@@ -1,5 +1,6 @@
 import random
 
+# TRAVAIL
 # Un genre de mélange entre un graphe et une linked list qui modélise le chemin des ennemis
 
 class Graph_node():
@@ -15,10 +16,15 @@ class Graph_node():
 		self.sorties = []
 
 	def ajout_sortie(self, node, proba=1):
+		"""
+		@param proba: La probabilité que cette sortie soit choisie lorsqu'on quitte la node (entre 0 et 1)
+		"""
 		self.sorties.append((node, proba))
 		return node
 
+	# Décide de la prochaine node vers laquelle le mob se dirigera.
 	def aller_prochain(self):
+		
 		if random.random() <= self.sorties[0][1]:
 			return self.sorties[0][0]
 		else:
